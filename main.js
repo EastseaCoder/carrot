d8be822c6be54ddb2b5affd034c901f987bab4cf
EastseaCoder <110013811+EastseaCoder@users.noreply.github.com>
2025-04-02 20:43:37 +0900

    JS-Implement carrot & bug interactions

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const playBtn = document.querySelector('.play__Btn');
-const carrot = document.querySelector('.game__item--carrot');
-const bug = document.querySelector('.game__item--bug');
 const itemsContainer = document.querySelector('.items__container');
 const result = document.querySelector('.result-actions');
-result.style.display = 'none';
+const counter = document.querySelector('.counter');
+const timers = document.querySelector('.timer');
 document.addEventListener('DOMContentLoaded', () => {
   playBtn.addEventListener('click', () => {
-    itemsContainer.innerHTML = createGameItems();
+    playBtn.innerHTML = `<i class="fa-solid fa-stop"></i>`;
+    result.style.display = 'none';
+    timerStart();
+    const createGameItem = createGameItems();
+    itemsContainer.innerHTML = createGameItem.content;
+
+    let carrotCount = createGameItem.count;
+    counter.innerText = carrotCount;
+    gameStart(carrotCount);
     const carrotItem = document.querySelectorAll('.game__item--carrot');
     const bugItem = document.querySelectorAll('.game__item--bug');
     setRandomPosition(carrotItem);
@@ -16,19 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-const getRandom = (max, min) => Math.floor(Math.random() * (max - min));
+function getRandom(max, min) {
+  return Math.floor(Math.random() * (max - min));
+}
 
 function createGameItems() {
-  let content = '';
-  for (let i = 0; i <= 5; i++) {
-    content += `<div class="game__item game__item--carrot">
+  const array = Array(10);
+  const obj = {
+    content: '',
+    count: 0,
+  };
+  for (let i = 0; i < array.length; i++) {
+    obj.content += `<div class="game__item game__item--carrot">
         <img src="img/carrot.png" alt="Carrot" />
       </div>
       <div class="game__item game__item--bug">
         <img src="img/bug.png" alt="Bug" />
       </div>`;
+    obj.count++;
   }
-  return content;
+  return obj;
 }
 
 function setRandomPosition(gameItem) {
@@ -37,8 +51,40 @@ function setRandomPosition(gameItem) {
   const containerheight = itemsContainer.getBoundingClientRect().height;
   const itemWidth = item.getBoundingClientRect().width;
   const itemHeight = item.getBoundingClientRect().height;
-  gameItem.forEach((carrots) => {
-    carrots.style.left = getRandom(containerWidth, itemWidth) + 'px';
-    carrots.style.top = getRandom(containerheight, itemHeight) + 'px';
+  gameItem.forEach((item) => {
+    item.style.left = getRandom(containerWidth, itemWidth) + 'px';
+    item.style.top = getRandom(containerheight, itemHeight) + 'px';
+  });
+}
+
+function timerStart() {
+  let sec = 10;
+  updateTimerText(sec);
+  const timer = setInterval(() => {
+    sec--;
+    if (sec < 0) {
+      clearInterval(timer);
+    } else {
+      updateTimerText(sec);
+    }
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  timers.textContent = `0:${time}`;
+}
+
+function gameStart(count) {
+  itemsContainer.addEventListener('click', (e) => {
+    const target = e.target;
+    const carrotElement = target.closest('.game__item--carrot');
+    const bugElement = target.closest('.game__item--bug');
+    if (bugElement) {
+      counter.innerText = count;
+    } else if (carrotElement) {
+      count--;
+      carrotElement.style.display = 'none';
+      counter.innerText = count;
+    }
   });
 }
