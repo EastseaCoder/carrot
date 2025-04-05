cc06129f15b21e0c34354a7a5bbb511e19d628f1
EastseaCoder <110013811+EastseaCoder@users.noreply.github.com>
2025-04-05 15:51:51 +0900

    JS-GameSound

--- main.js
+++ main.js
@@ -3,25 +3,59 @@ const mainContainer = document.querySelector('.background__container');
 const itemsContainer = document.querySelector('.items__container');
 const counter = document.querySelector('.counter');
 const timers = document.querySelector('.timer');
+
 let currentTimer = null;
 let gameHandler = null;
+
 const btnState = {
   isPlaying: false,
   setIsPlaying(value) {
     this.isPlaying = value;
   },
 };
+const gameAudio = {
+  bgAudio: new Audio('/sound/bg.mp3'),
+  gameResetSound: new Audio('/sound/alert.wav'),
+  bugClickSound: new Audio('/sound/bug_pull.mp3'),
+  carrotClickSound: new Audio('/sound/carrot_pull.mp3'),
+  gameWinSound: new Audio('/sound/game_win.mp3'),
+};
+for (let key in gameAudio) {
+  gameAudio[key].preload = 'auto';
+  gameAudio[key].load();
+}
 
+function playSound(name) {
+  const sound = gameAudio[name];
+  if (!sound) return;
+  if (name === 'bgAudio') {
+    sound.loop = 'true';
+  }
+  sound.pause();
+  sound.currentTime = 0;
+  sound.volume = 0.5;
+  sound.play();
+}
+
+function stopSound(name) {
+  const sound = gameAudio[name];
+  if (!sound) return;
+  sound.pause();
+  sound.currentTime = 0;
+}
 document.addEventListener('DOMContentLoaded', () => {
   mainContainer.addEventListener('click', (e) => {
     const PlayBtn = e.target.closest('.play__btn');
     const resetBtn = e.target.closest('.reset__btn');
     if (PlayBtn) {
       if (!btnState.isPlaying) {
+        playSound('bgAudio');
         PlayBtn.innerHTML = `<i class="fa-solid fa-stop"></i>`;
         gameUpdate();
         btnState.setIsPlaying(true);
       } else {
+        stopSound('bgAudio');
+        playSound('gameResetSound');
         PlayBtn.style.opacity = 0;
         if (gameHandler) {
           itemsContainer.removeEventListener('click', gameHandler);
@@ -29,6 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         gameResult('Retry?', currentTimer);
       }
     } else if (resetBtn) {
+      playSound('bgAudio');
       const mainBtn = document.querySelector('.main__btn');
       mainBtn.style.opacity = 1;
       gameUpdate();
@@ -101,16 +136,21 @@ function gameStart(count) {
 
     if (bugElement) {
       gameResult('YOU LOSE', gameTimer.timer);
+      stopSound('bgAudio');
+      playSound('bugClickSound');
       isGameFinished = true;
       Btn.style.opacity = 0;
     } else if (carrotElement) {
       count--;
       carrotElement.style.display = 'none';
       counter.innerText = count;
+      gameAudio.carrotClickSound.play();
       if (count === 0) {
         gameResult('YOU WON', gameTimer.timer);
         isGameFinished = true;
         Btn.style.opacity = 0;
+        stopSound('bgAudio');
+        playSound('gameWinSound');
       }
     }
   };
@@ -127,6 +167,8 @@ function timerStart() {
     if (sec < 0) {
       gameResult('YOU LOSE', timer);
       isTimerFinished = true;
+      stopSound('bgAudio');
+      playSound('gameResetSound');
     } else {
       timers.textContent = `0:${sec}`;
     }
